801eb5112b54a5be18ba862c8b0a5e6e07ac8cf8
iverenshaguy <iverenshaguy@gmail.com>
2017-04-01 16:34:36 +0100

    all my code

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$server = "localhost";
+$db_user = "root";
+$db_pass = "mysql";
+$db_name = "crud";
+
+$connection = mysqli_connect($server, $db_user, $db_pass, $db_name);
---
+++ links.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+
+<h3 style="text-align:center">
+<a href="form.php">Register</a>
+<a href="update.php">Edit User</a>
+<a href="delete.php">Delete User</a>
+<a href="search.php">Search User</a>
+
+
+</h3>
